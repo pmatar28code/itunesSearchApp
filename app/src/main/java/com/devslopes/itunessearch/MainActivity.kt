cb084b5f4b420a65b3d32876b6cd8c4d80592c18
pmatar28code <78227811+pmatar28code@users.noreply.github.com>
2021-03-23 14:27:31 -0600

    fab working, showing and hiding on search, dont need to press radio buttons again
    fab working, showing and hiding on search, dont need to press radio buttons again.

--- app/src/main/java/com/devslopes/itunessearch/MainActivity.kt
+++ app/src/main/java/com/devslopes/itunessearch/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.devslopes.itunessearch
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
 import android.os.Handler
+import android.view.KeyEvent
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.devslopes.itunessearch.databinding.ActivityMainBinding
 import com.devslopes.itunessearch.repositories.ItunesRepository
@@ -26,6 +29,40 @@ class MainActivity : AppCompatActivity() {
             1000 // value in milliseconds
         )
 
+        binding.search.setOnQueryTextListener(object: androidx.appcompat.widget.SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                binding.test.text = "again"
+                var searchFabButton = binding.floatingActionButton2
+                searchFabButton.hide()
+                return true
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                binding.test.text = "pedro"
+                var searchFabButtonShow = binding.floatingActionButton2
+                searchFabButtonShow.show()
+                return true
+            }
+
+        })
+
+        binding.floatingActionButton2.setOnClickListener {
+            val imm: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+            fetchMatchingItems(binding,storeItemAdapter)
+            Handler().postDelayed(
+                    {
+                        storeItemAdapter.notifyDataSetChanged()                    },
+                    2000 // value in milliseconds
+            )
+           // storeItemAdapter.notifyDataSetChanged()
+
+            binding.floatingActionButton2.hide()
+            KeyEvent.KEYCODE_ENTER
+            binding.search.setOnSearchClickListener{}
+
+        }
+
 
         binding.apply {
             results.apply {
@@ -34,19 +71,23 @@ class MainActivity : AppCompatActivity() {
                 layoutManager = LinearLayoutManager(this@MainActivity)
             }
 
-            search.setOnSearchClickListener { 
-                fetchMatchingItems(binding, storeItemAdapter)
+            search.setOnSearchClickListener {
+               // ItunesRepository.moviesServerMutable.clear()
+                //storeItemAdapter.notifyDataSetChanged()
+                //fetchMatchingItems(binding, storeItemAdapter)
+
                // storeItemAdapter.submitList(ItunesRepository.moviesTitles)
                 //storeItemAdapter.notifyDataSetChanged()
-                ItunesRepository.moviesServerString.clear()
-                storeItemAdapter.notifyDataSetChanged()
-               storeItemAdapter.submitList(ItunesRepository.moviesServerMutable)
-                storeItemAdapter.notifyDataSetChanged()
+                //ItunesRepository.moviesServerString.clear()
+                //storeItemAdapter.notifyDataSetChanged()
+               //storeItemAdapter.submitList(ItunesRepository.moviesServerMutable)
+                //storeItemAdapter.notifyDataSetChanged()
 
             }
 
             filter.setOnCheckedChangeListener { _, _ ->
-                fetchMatchingItems(binding, storeItemAdapter)
+               // fetchMatchingItems(binding, storeItemAdapter)
+
                // storeItemAdapter.submitList(ItunesRepository.moviesServerString)
                // storeItemAdapter.notifyDataSetChanged()
 
@@ -69,8 +110,11 @@ class MainActivity : AppCompatActivity() {
             // perform search!!!
             ItunesRepository.moviesServerMutable.clear()
             when(mediaType){
-                "movie" -> ItunesRepository.callGetMovie(this,"movie",searchTerm)
+
+                "movie" -> {ItunesRepository.callGetMovie(this,"movie",searchTerm);storeItemAdapter.submitList(ItunesRepository.moviesServerMutable);storeItemAdapter.notifyDataSetChanged()}
+
             }
+
 
 
         }
